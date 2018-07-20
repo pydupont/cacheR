7c701cb3dbe93d9919b314efb8b68dcb2b2febcf
Pierre-Yves Dupont <pierre.dupont@esr.cri.nz>
2018-07-20 15:24:11 +1200

    Added documentation on the code

--- R/caching.R
+++ R/caching.R
@@ -1,5 +1,12 @@
 library(assertthat)
 
+#' Save an object (cache)
+#'
+#' @param o object to save
+#' @return nothing
+#' @export
+#' @examples
+#' save_object(iris)
 save_object <- function(o){
   cache.dir <- ".cache"
   dir.create(cache.dir, showWarnings = FALSE) #file.path(mainDir, subDir)
@@ -9,6 +16,15 @@ save_object <- function(o){
   write(paste("Object ", name, " saved", sep=""), stderr())
 }
 
+#' Load an object (cache)
+#'
+#' @param name name of the object to load
+#' @param global load in the global environment (if TRUE) or as an object (if FALSE, default)
+#' @return object or nothing
+#' @export
+#' @examples
+#' load_object("iris", global=T)
+#' iris2 <- load_object("iris")
 load_object <- function(name, global=F){
   if (is.null(global)){global=F}
   cache.dir <- ".cache"
@@ -27,6 +43,13 @@ load_object <- function(name, global=F){
   }
 }
 
+#' Check if an object is cached
+#'
+#' @param name name of the object to search
+#' @return bool TRUE if object is cached, else FALSE
+#' @export
+#' @examples
+#' file.cached("iris")
 file.cached <- function(name){
   cache.dir <- ".cache"
   if(!is.string(name)){return(F)}
@@ -34,10 +57,17 @@ file.cached <- function(name){
   return(F)
 }
 
+#' For assertthat library. What to do if assert_that(file.cached("iris")) fails
 on_failure(file.cached) <- function(call, env) {
   "File is not cached"
 }
 
+#' Return a list of the cached files
+#'
+#' @return vector of the names of the cached objects (as strings)
+#' @export
+#' @examples
+#' get.file.cached()
 get.file.cached <- function(){
   cache.dir <- ".cache"
   assert_that(dir.exists(cache.dir))
@@ -49,6 +79,12 @@ get.file.cached <- function(){
   return(objects)
 }
 
+#' Loads all cached objects in the global environment. Used to restore an envirnoment quickly
+#'
+#' @return nothing
+#' @export
+#' @examples
+#' load.all.cached.files()
 load.all.cached.files <- function(){
   for(x in get.file.cached()){
     load_object(x, global=T)
